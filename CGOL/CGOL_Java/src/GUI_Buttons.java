87d30e812480817966935311b1ebfa4bae3e6266
mraddtab <70644931+mraddtab@users.noreply.github.com>
2021-11-28 20:25:58 -0500

    Delete CGOL/CGOL_Java directory

--- CGOL/CGOL_Java/src/GUI_Buttons.java
+++
@@ -1,9 +0,0 @@
-public class GUI_Buttons {
-    public static void main(String[] args) {
-        //JButton = a button that performs an action when clicked on
-
-        GUI_MyFrameButton FRAME = new GUI_MyFrameButton();
-
-
-    }
-}
